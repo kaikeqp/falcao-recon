da75962388261d46e0770a44602d235c02d679d2
Kaike Ferreira <kaikemice@gmail.com>
2025-03-01 00:23:22 -0300

    a

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ CONFIG = {
     "escala_frame": 0.5,
     "use_gpu": False,  # Alterado para False para usar CPU
     "det_size": (640, 640),
-    "model_path": os.path.join("models", "buffalo_l"),
+    "model_path": os.path.join(""),
 }
 
 # =============== INICIALIZAÇÃO DO INSIGHTFACE ===============
