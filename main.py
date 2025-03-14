7a6dab631549017361e58c1c11b6dc5d0d20c223
Kaike Ferreira <kaikemice@gmail.com>
2025-03-13 21:34:03 -0300

    path model

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ CONFIG = {
     "escala_frame": 0.5,
     "use_gpu": False,  # Alterado para False para usar CPU
     "det_size": (640, 640),
-    "model_path": "",
+    "model_path": "/home/ubuntu/.insightface",
 }
 
 # =============== INICIALIZAÇÃO DO INSIGHTFACE ===============
